c61da4d8986c7a280b843c9f118f1693738bb2e8
Shanly Suepaul <shanlys@gmail.com>
2016-05-09 22:26:35 +0100

    naive implementation of passenger functionality

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 (function() {
     var app = angular.module('flightSelector', []);
 
-    app.controller('FlightSelectorController', function() {
+    app.controller('FlightSelectorController', [
+        '$scope',
+        function(
+            $scope
+        ) {
+            $scope.step = 1;
+            $scope.passengers = {
+                adults: 1,
+                children: 0,
+                infants: 0
+            };
 
-    });
+            $scope.submitPassengerCount = function () {
+                if ((parseInt($scope.passengers.adults) + parseInt($scope.passengers.children) + parseInt($scope.passengers.infants)) <= 0) {
+                    alert('You must send at least one person!');
+                    return;
+                }
+
+                $scope.step = 2;
+            };
+        }
+    ]);
+
 })();
