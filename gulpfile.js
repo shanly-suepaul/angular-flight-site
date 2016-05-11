08275fec9d419015469cc63185d6b64016ef3907
Shanly Suepaul <shanlys@gmail.com>
2016-05-11 12:12:53 +0100

    Adding sass style compilation to serve task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
 
 var es = require('event-stream');
+var sass = require('gulp-sass');
 var jshint = require('gulp-jshint');
 var serve = require('gulp-serve');
 var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 var livereload = require('gulp-livereload');
 var connectLivereload = require('connect-livereload');
+var rimraf = require('rimraf');
+
+function getCompiledStylesheetAsset(ignoreErrors) {
+    return gulp.src('src/**/*.scss')
+        .pipe(sass().on('error', function () {}))
+        .pipe(concat('app.css'));
+}
 
 gulp.task('serve', function () {
+    var previewTmpDir = '.tmp/dev-preview';
+
     livereload.listen({
         host: 'localhost'
     });
@@ -22,21 +34,40 @@ gulp.task('serve', function () {
         watch([
             'src/**/*.{html,json}',
             'assets/**/*.{png,jpg,jpeg,gif,webp,svg}'
-        ])
+        ]),
+
+        // rebuild entire compiled stylesheet
+        watch('src/**/*.scss').pipe(es.map(function (e, cb) {
+            getCompiledStylesheetAsset(true) // ignore errors
+                .pipe(rename({ dirname: 'assets' }))
+                .pipe(gulp.dest(previewTmpDir))
+                .on('data', cb.bind(null, null));
+        }))
     ).pipe(livereload());
 
-    // listen on multiple ports
-    [ 9000, 9001 ].forEach(function (port) {
-        serve({
-            port: port,
+    rimraf(previewTmpDir, function () {
+        var tempFiles = es.concat(
+            getCompiledStylesheetAsset()
+        ).pipe(rename(function (path) {
+            path.dirname = 'assets/' + path.dirname;
+        })).pipe(gulp.dest(previewTmpDir));
 
-            middlewares: [
-                connectLivereload()
-            ],
+        tempFiles.on('end', function () {
+            // listen on multiple ports
+            [9000, 9001].forEach(function (port) {
+                serve({
+                    port: port,
 
-            root: [
-                __dirname
-            ]
-        })();
+                    middlewares: [
+                        connectLivereload()
+                    ],
+
+                    root: [
+                        __dirname + '/' + previewTmpDir,
+                        __dirname
+                    ]
+                })();
+            });
+        });
     });
 });
